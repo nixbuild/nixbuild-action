56cf3ef2f616e10cbc4a6bc596b3197bb74e8474
Rickard Nilsson <rickynils@gmail.com>
2021-10-01 10:58:05 +0200

    Enable builds that require kvm or nixos-test features

--- nixbuild-action.sh
+++ nixbuild-action.sh
@@ -78,7 +78,7 @@ sudo tee -a /etc/ssh/ssh_config < "$SSH_CONFIG_FILE" >/dev/null
 # Setup Nix builders
 NIX_BUILDERS_FILE="$(mktemp)"
 cat >"$NIX_BUILDERS_FILE" <<EOF
-eu.nixbuild.net x86_64-linux - 200 1 big-parallel,benchmark
+eu.nixbuild.net x86_64-linux - 200 1 big-parallel,benchmark,kvm,nixos-test
 eu.nixbuild.net aarch64-linux - 200 1 big-parallel,benchmark
 EOF
 
