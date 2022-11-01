2a8fe1eda06e43310640641ab9ded487ed8e7579
Rickard Nilsson <rickynils@gmail.com>
2022-11-01 17:45:15 +0100

    summary.js: Use the GitHub summary API

--- summary.js
+++ summary.js
@@ -3,6 +3,16 @@ const github = require('@actions/github');
 const child_process = require('child_process');
 const https = require('https');
 
+// https://stackoverflow.com/a/18650828
+function formatBytes(bytes, decimals = 2) {
+  if (!+bytes) return '0 KB'
+  const k = 1024
+  const dm = decimals < 0 ? 0 : decimals
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+  const i = Math.floor(Math.log(bytes) / Math.log(k))
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
+}
+
 try {
   // If the shell command fails we will get an exception,
   // causing the post step to fail
@@ -14,7 +24,8 @@ try {
   const repository = process.env.GITHUB_REPOSITORY;
   const run_id = process.env.GITHUB_RUN_ID;
   const run_attempt = process.env.GITHUB_RUN_ATTEMPT;
-  const path = `/builds/summary?tags=GITHUB_REPOSITORY:${repository},GITHUB_RUN_ID:${run_id},GITHUB_RUN_ATTEMPT:${run_attempt}`;
+  const job = process.env.GITHUB_JOB;
+  const path = `/builds/summary?tags=GITHUB_REPOSITORY:${repository},GITHUB_RUN_ID:${run_id},GITHUB_RUN_ATTEMPT:${run_attempt},GITHUB_JOB:${job}`;
   const options = {
     host: 'api.nixbuild.net',
     path: path,
@@ -32,6 +43,16 @@ try {
       } else {
         var summary = JSON.parse(body);
         core.info(JSON.stringify(summary));
+        core.summary
+          .addHeading('nixbuild.net build summary')
+          .addTable([
+            ['&#x2714;', 'Successful builds', summary.successful_build_count.toString()],
+            ['&#x274C;', 'Failed builds', summary.failed_build_count.toString()],
+            ['&#x1F3F4;', 'Restarted builds', summary.discarded_build_count.toString()],
+            ['&#x23F1;', 'Billable CPU hours', (summary.billable_cpu_seconds / 3600.0).toFixed(2)],
+            ['&#x1F4E6;', 'Total output size', formatBytes(1024 * summary.total_output_nar_size_kilobytes)]
+          ])
+          .write()
       }
     });
   });
@@ -39,7 +60,6 @@ try {
     core.warning(`Error related to HTTPS request: ${err}`);
   });
   request.end();
-
 } catch (error) {
   core.setFailed(error.message);
 }
