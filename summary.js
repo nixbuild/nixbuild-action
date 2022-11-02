cf0e62a347aca830e8686e695ba9f329d0950ed8
Rickard Nilsson <rickynils@gmail.com>
2022-11-02 23:32:22 +0100

    summary: Add a list of built derivations

--- summary.js
+++ summary.js
@@ -27,52 +27,74 @@ function generateSummary(token, allJobs) {
   const run_attempt = process.env.GITHUB_RUN_ATTEMPT;
   const job = process.env.GITHUB_JOB;
   const invocation_id = fs.readFileSync(path.resolve(process.env.HOME, '__nixbuildnet_invocation_id'));
-  var p = `/builds/summary?tags=GITHUB_REPOSITORY:${repository}`;
-  if (allJobs) {
-    p += `,GITHUB_RUN_ID:${run_id},GITHUB_RUN_ATTEMPT:${run_attempt}`;
-  } else {
-    p += `,GITHUB_INVOCATION_ID:${invocation_id}`;
-  }
-  core.info(`api query: ${p}`);
-  const options = {
+  const queryParams = allJobs ?
+    `tags=GITHUB_REPOSITORY:${repository},GITHUB_RUN_ID:${run_id},GITHUB_RUN_ATTEMPT:${run_attempt}` :
+    `tags=GITHUB_REPOSITORY:${repository},GITHUB_INVOCATION_ID:${invocation_id}`;
+  const summaryOpts = {
     host: 'api.nixbuild.net',
-    path: p,
+    path: '/builds/summary?' + queryParams,
     method: 'GET',
     headers: {'Authorization': 'Bearer ' + token}
   };
-  const request = https.request(options, function (response) {
-    var body = '';
-    response.on('data', function (chunk) {
-      body += chunk;
+  const summaryReq = https.request(summaryOpts, function (summaryRes) {
+    var summaryBody = '';
+    summaryRes.on('data', function (chunk) {
+      summaryBody += chunk;
     });
-    response.on('end', function () {
-      if (response.statusCode != 200) {
-        core.setFailed(`nixbuild.net API returned: ${body}`);
+    summaryRes.on('end', function () {
+      if (summaryRes.statusCode != 200) {
+        core.warning(`nixbuild.net API returned: ${summaryBody}`);
       } else {
-        var summary = JSON.parse(body);
-        var heading = '';
-        if (allJobs) {
-          heading = 'Total <a href="https://nixbuild.net/">nixbuild.net</a> usage';
+        const s = JSON.parse(summaryBody);
+        if (s.build_count > 0) {
+          const buildsOpts = { ...summaryOpts, path: '/builds?' + queryParams };
+          const buildsReq = https.request(buildsOpts, function (buildsRes) {
+            var buildsBody = '';
+            buildsRes.on('data', function (chunk) {
+              buildsBody += chunk;
+            });
+            buildsRes.on('end', function () {
+              if (buildsRes.statusCode != 200) {
+                core.warning(`nixbuild.net API returned: ${buildsBody}`);
+              } else {
+                writeSummary(allJobs, s, JSON.parse(buildsBody));
+              }
+            });
+          });
+          buildsReq.on('error', function (err) {
+            core.warning(`Error related to HTTPS request: ${err}`);
+          });
+          buildsReq.end();
         } else {
-          heading = '<a href="https://nixbuild.net/">nixbuild.net</a> usage for this job';
+          writeSummary(allJobs, s, []);
         }
-        core.summary
-          .addHeading(heading, 3)
-          .addTable([
-            ['&#x2714;', 'Successful builds', summary.successful_build_count.toString()],
-            ['&#x274C;', 'Failed builds', summary.failed_build_count.toString()],
-            ['&#x1F3F4;', 'Restarted builds', summary.discarded_build_count.toString()],
-            ['&#x23F1;', 'Billable CPU hours', (summary.billable_cpu_seconds / 3600.0).toFixed(2)],
-            ['&#x1F4E6;', 'Total output size', formatBytes(1024 * summary.total_output_nar_size_kilobytes)]
-          ])
-          .write()
-      }
+      };
     });
   });
-  request.on('error', function (err) {
+  summaryReq.on('error', function (err) {
     core.warning(`Error related to HTTPS request: ${err}`);
   });
-  request.end();
+  summaryReq.end();
+}
+
+function writeSummary(allJobs, s, builds) {
+  const heading = allJobs ?
+    '<a href="https://nixbuild.net/">nixbuild.net</a> summary for workflow' :
+    '<a href="https://nixbuild.net/">nixbuild.net</a> summary for this job';
+  const summary = core.summary
+    .addHeading(heading, 3)
+    .addTable([
+      ['&#x2714;', 'Successful builds', s.successful_build_count.toString()],
+      ['&#x274C;', 'Failed builds', s.failed_build_count.toString()],
+      ['&#x1F3F4;', 'Restarted builds', s.discarded_build_count.toString()],
+      ['&#x23F1;', 'Billable CPU hours', (s.billable_cpu_seconds / 3600.0).toFixed(2)],
+      ['&#x1F4E6;', 'Total output size', formatBytes(1024 * s.total_output_nar_size_kilobytes)]
+    ]);
+  if (s.build_count > 0) {
+    summary.addHeading('Builds', 4);
+    summary.addTable(builds.map(b => [b.derivation_path]));
+  };
+  summary.write();
 }
 
 const summaryFor = core.getInput('generate-summary-for').toLowerCase();
