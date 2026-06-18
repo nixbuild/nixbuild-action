d3d87928f4418adaa3f61ec5ccf8ea7000e77d9a
Rickard Nilsson <rickynils@gmail.com>
2026-06-18 12:12:11 +0200

    When OIDC is enabled, always do the token exchange

--- nixbuild-action.sh
+++ nixbuild-action.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -x
-
 set -eu
 set -o pipefail
 
@@ -115,21 +113,20 @@ if [ "$(get_input OIDC)" = "true" ]; then
       exit 1
     else
       echo "NIXBUILDNET_OIDC_ID_TOKEN=$NIXBUILDNET_OIDC_ID_TOKEN" >> "$GITHUB_ENV"
-      if [ "$(printenv INPUTS_JSON | jq -r .OIDC_TOKEN_EXCHANGE)" = "true" ]; then
-        base_url="$NIXBUILDNET_HTTP_API_SCHEME://$NIXBUILDNET_HTTP_API_HOST:$NIXBUILDNET_HTTP_API_PORT$NIXBUILDNET_HTTP_API_SUBPATH"
-        NEW_NIXBUILDNET_TOKEN="$(curl --fail-with-body -L \
-          "$base_url/auth/oidc-token-exchange" \
-          -H "Authorization: Bearer $NIXBUILDNET_TOKEN" \
-          -H "NIXBUILDNET-OIDC-ID-TOKEN: $NIXBUILDNET_OIDC_ID_TOKEN" \
-          -H "Content-Type: application/json"
-        )"
-        if [ -z "${NEW_NIXBUILDNET_TOKEN+x}" ]; then
-          echo >&2 "Failed to exchange OIDC token. Keeping the old nixbuild token."
-        else
-          NIXBUILDNET_TOKEN="$NEW_NIXBUILDNET_TOKEN"
-          echo "NIXBUILDNET_TOKEN=$NIXBUILDNET_TOKEN" >> "$GITHUB_ENV"
-          echo >&2 "Token exchange successful."
-        fi
+      base_url="$NIXBUILDNET_HTTP_API_SCHEME://$NIXBUILDNET_HTTP_API_HOST:$NIXBUILDNET_HTTP_API_PORT$NIXBUILDNET_HTTP_API_SUBPATH"
+      NEW_NIXBUILDNET_TOKEN="$(curl -sSL \
+        --fail-with-body \
+        "$base_url/auth/oidc-token-exchange" \
+        -H "Authorization: Bearer $NIXBUILDNET_TOKEN" \
+        -H "NIXBUILDNET-OIDC-ID-TOKEN: $NIXBUILDNET_OIDC_ID_TOKEN" \
+        -H "Content-Type: application/json"
+      )"
+      if [ -z "${NEW_NIXBUILDNET_TOKEN+x}" ]; then
+        echo >&2 "Failed to exchange OIDC token. Keeping the old nixbuild token."
+      else
+        NIXBUILDNET_TOKEN="$NEW_NIXBUILDNET_TOKEN"
+        echo "NIXBUILDNET_TOKEN=$NIXBUILDNET_TOKEN" >> "$GITHUB_ENV"
+        echo >&2 "Token exchange successful."
       fi
     fi
   fi
